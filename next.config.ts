421a23bf3c0e50c77e75955f5ac11404c598df65
shaoneng <wushaoneng@qq.com>
2025-08-05 14:46:14 +0800

    fix: Force push of final next.config.mjs for build

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 这是新增的配置
   experimental: {
     // 这个选项会改变Next.js打包（bundle）大型库的方式
     // 从而避免生成单个过大的文件，以适应Cloudflare等平台的限制
-    largePageDataBytes: 128 * 1000, // 128kb (这是一个推荐的默认值)
+    largePageDataBytes: 128 * 1000, // 128kb
   },
 };
 
